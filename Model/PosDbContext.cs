c0087f4e0a791d4b6807814829c1a3fba60703cf
sreng-step <sreng.itstep@gmail.com>
2022-07-12 20:38:53 +0700

    Update PosDbContext.cs

--- Model/PosDbContext.cs
+++ Model/PosDbContext.cs
@@ -22,7 +22,7 @@ namespace Point_of_Sales.Model
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(Configuration);
+                //optionsBuilder.UseSqlServer(Configuration);
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=POS.mdf;Integrated Security=True;Connect Timeout=30");
             }
