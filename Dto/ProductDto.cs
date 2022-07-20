7d4da4e93092f2ccb98b7c13a4fc13e5ffbe36af
SamnangPheng <phengsamnangsp@gmail.com>
2022-07-20 16:04:54 +0700

    Add ProductDto

---
+++ Dto/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point_of_Sales.Dto
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost_price { get; set; }
+        public decimal Selling_price { get; set; }
+        public string Unit { get; set; }
+    }
+}
