cfba931530d57b382293a74b981a7e1195c6a976
SamnangPheng <phengsamnangsp@gmail.com>
2022-07-26 18:17:33 +0700

    Add_Login_and_ForgotPassword_ViewModel

---
+++ Back_end/ViewModel/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point_of_Sales.Back_end.ViewModel
+{
+    public class ForgotPasswordViewModel : ViewModelBase
+    {
+
+    }
+}
